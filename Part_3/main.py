06c581fc0deb93cb9d0aad7df307a5435bd6cb1a
Bruno Danti <bruno.danti@sns.it>
2025-10-03 03:04:22 +0200

    CLI converted Part

--- Part_3/main.py
+++ Part_3/main.py
@@ -1,38 +1,8 @@
-import numpy as np
+import numpy as np, sys
 from scipy.linalg.blas import dsyrk
-from timeit import default_timer as timer
 
+n_images = int(sys.argv[1])
+G: np.ndarray = np.loadtxt(sys.argv[2], float, delimiter=',', max_rows=n_images)
+K: np.ndarray = dsyrk(alpha = 1.0, a = G, lower = False)
 
-t0 = timer()
-
-G: np.ndarray = np.loadtxt("../Part_2/G_matrix.csv",
-                           float, delimiter= ',')
-
-
-G = G[0:3000, :]
-
-t1 = timer()
-
-print(f"Time to load matrix: {t1 - t0}")
-print(G.shape)
-print(G[0:4, 0:5])
-
-#K: np.ndarray = dsyrk(alpha = 1.0, a = G, lower = False, overwrite_c = False)
-K: np.ndarray = G @ G.T
-
-t2 = timer()
-
-print(f"Time to multiply G @ G.T: {t2 - t1}")
-print(K.shape)
-print(type(K))
-print(K[0:4, 0:4])
-
-
-print(
-    f"Rank of K:\n",
-    np.linalg.matrix_rank(K)
-)
-
-t3 = timer()
-
-print(f"Time to compute rank = {t3 - t2}")
+np.savetxt(sys.argv[3], K, delimiter= ",")
