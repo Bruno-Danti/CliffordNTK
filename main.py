5ed66d6d26ec36783576fa4ffe55fdb4c8b3e0c4
Bruno Danti <bruno.danti@sns.it>
2025-11-03 18:17:05 +0100

    20251103

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from perform_experiment import perform_experiment
 
-perform_experiment(
-    10, 20034, 10000,
-    
-    10, 3, 252,
-    "./data/download/train.bin",
-    "./data/download/test.bin",
-    "./data/tmp/paulis.bin",
-    
-    "./data/out/K_train_train_1.csv", "./data/out/K_test_train_1.csv"
-)
+n_train_images = 30_000
+n_test_images = 10_000
+run = lambda n_samples,\
+             K_tr_tr_path,\
+             K_ts_tr_path:\
+            perform_experiment(
+                n_samples, n_train_images, n_test_images,
+                10, 3, 252,
+                "./data/download/train.bin",
+                "./data/download/test.bin",
+                "./data/tmp/paulis.bin",
+                K_tr_tr_path, K_ts_tr_path
+            )
+
+
+for i in range(3):
+    n_samples = 32 * (2**i)
+    run(n_samples,
+        f"./data/out/K_train_train_{n_samples}_samples_{n_train_images}x{n_train_images}",
+        f"./data/out/K_test_train_{n_samples}_samples_{n_test_images}x{n_train_images}")
